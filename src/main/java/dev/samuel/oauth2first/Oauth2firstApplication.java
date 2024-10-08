084419c8588fb83634be1beac5df4cd9794a8876
samuelgbenga <samuelgbenga972@gmail.com>
2024-10-08 05:54:12 +0100

    completed

---
+++ src/main/java/dev/samuel/oauth2first/Oauth2firstApplication.java
@@ -0,0 +1,13 @@
+package dev.samuel.oauth2first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2firstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2firstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/samuel/oauth2first/Oauth2firstApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.samuel.oauth2first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2firstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
